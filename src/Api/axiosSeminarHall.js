e2d17028ee64bd28f6f9ae55f1f03548e069579e
mohammedanaz <mohammedanaz@gmail.com>
2024-07-22 11:48:50 +0530

    base url in axios instance is changed to use url in .env file or if not provided then use localhost url at port 8000 for develepment server

--- src/Api/axiosSeminarHall.js
+++ src/Api/axiosSeminarHall.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8000/';
+
 export const seminarHallAxios = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: baseUrl,
     headers: { 'Content-Type': 'application/json' },
   });
 
